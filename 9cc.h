6fa1ef9d44a3bbdb4b2a2012a5242603a5823fe8
Toru Takefusa <torutakefusa@gmail.com>
2022-10-24 21:44:37 +0900

    ソースコードを分割

---
+++ 9cc.h
@@ -0,0 +1,59 @@
+//
+// Created by toru on 22/10/24.
+//
+
+#ifndef INC_9CC_9CC_H
+#define INC_9CC_9CC_H
+
+// トークンの種類
+typedef enum {
+    TK_RESERVED, // 記号
+    TK_NUM,      // 整数トークン
+    TK_EOF,      // 入力の終わりを表すトークン
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token {
+    TokenKind kind; // トークンの型
+    Token *next;    // 次の入力トークン
+    int val;        // kindがTK_NUMの場合、その数値
+    char *str;      // トークン文字列
+    int len;
+};
+
+// 現在着目しているトークン
+extern Token *token;
+
+typedef enum {
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_MUL, // *
+    ND_DIV, // /
+    ND_EQ, // ==
+    ND_NE, // !=
+    ND_LT, // <
+    ND_LE, // <=
+    ND_NUM, //整数
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+    NodeKind kind; // ノードの型
+    Node *lhs; // 左辺
+    Node *rhs; // 右辺
+    int val;   // kindがND_NUMの場合のみ使う
+};
+
+// 入力プログラム
+extern char *user_input;
+
+void error(char *fmt, ...);
+Token *tokenize();
+Node *expr();
+
+void gen(Node *node);
+#endif //INC_9CC_9CC_H
+
+
--- parse.c
+++ parse.c
@@ -1,32 +1,10 @@
-#include <ctype.h>
+#include <string.h>
 #include <stdarg.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-// トークンの種類
-typedef enum {
-    TK_RESERVED, // 記号
-    TK_NUM,      // 整数トークン
-    TK_EOF,      // 入力の終わりを表すトークン
-} TokenKind;
-
-typedef struct Token Token;
-
-struct Token {
-    TokenKind kind; // トークンの型
-    Token *next;    // 次の入力トークン
-    int val;        // kindがTK_NUMの場合、その数値
-    char *str;      // トークン文字列
-    int len;
-};
-
-// 入力プログラム
-char *user_input;
-
-// 現在着目しているトークン
-Token *token;
+#include <stdbool.h>
+#include <ctype.h>
+#include "9cc.h"
 
 // エラーを報告しているトークン
 // printfと同じ引数を取る
@@ -100,7 +78,6 @@ bool startswith(char *p, char *q) {
     return memcmp(p, q, strlen(q)) == 0;
 }
 
-
 // 入力文字列pをトークナイズしてそれを返す
 Token *tokenize() {
     char *p = user_input;
@@ -142,27 +119,6 @@ Token *tokenize() {
     return head.next;
 }
 
-typedef enum {
-    ND_ADD, // +
-    ND_SUB, // -
-    ND_MUL, // *
-    ND_DIV, // /
-    ND_EQ, // ==
-    ND_NE, // !=
-    ND_LT, // <
-    ND_LE, // <=
-    ND_NUM, //整数
-} NodeKind;
-
-typedef struct Node Node;
-
-struct Node {
-    NodeKind kind; // ノードの型
-    Node *lhs; // 左辺
-    Node *rhs; // 右辺
-    int val;   // kindがND_NUMの場合のみ使う
-};
-
 Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
     Node *node = calloc(1, sizeof(Node));
     node->kind = kind;
@@ -177,8 +133,6 @@ Node *new_node_num(int val) {
     node->val = val;
     return node;
 }
-
-Node *expr();
 
 Node *equality();
 
@@ -271,81 +225,4 @@ Node *primary() {
     }
 
     return new_node_num(expect_number());
-}
-
-void gen(Node *node) {
-    if (node->kind == ND_NUM) {
-        printf("    push %d\n", node->val);
-        return;
-    }
-
-    gen(node->lhs);
-    gen(node->rhs);
-
-    printf("    pop rdi\n");
-    printf("    pop rax\n");
-
-    switch (node->kind) {
-        case ND_ADD:
-            printf("    add rax, rdi\n");
-            break;
-        case ND_SUB:
-            printf("    sub rax, rdi\n");
-            break;
-        case ND_MUL:
-            printf("    imul rax, rdi\n");
-            break;
-        case ND_DIV:
-            printf("    cqo\n");
-            printf("    idiv rdi\n");
-            break;
-        case ND_EQ:
-            printf("    cmp rax, rdi\n");
-            printf("    sete al\n");
-            printf("    movzb rax, al\n");
-            break;
-        case ND_LT:
-            printf("    cmp rax, rdi\n");
-            printf("    setl al\n");
-            printf("    movzb rax, al\n");
-            break;
-        case ND_LE:
-            printf("    cmp rax, rdi\n");
-            printf("    setle al\n");
-            printf("    movzb rax, al\n");
-            break;
-        case ND_NE:
-            printf("    cmp rax, rdi\n");
-            printf("    setne al\n");
-            printf("    movzb rax, al\n");
-            break;
-    }
-
-    printf("    push rax\n");
-}
-
-int main(int argc, char **argv) {
-    if (argc != 2) {
-        error("引数の個数が正しくありません。");
-        return 1;
-    }
-
-    // トークナイズしてパースする。
-    user_input = argv[1];
-    token = tokenize();
-    Node *node = expr();
-
-    // アセンブリの前半部分を出力
-    printf(".intel_syntax noprefix\n");
-    printf(".global main\n");
-    printf("main:\n");
-
-    // 抽象構文木を下りながらコード生成
-    gen(node);
-
-    // スタックトップに式全体の値が残っているはずなので、
-    // それをRAXにロードして関数からの返り値とする
-    printf("  pop rax\n");
-    printf("  ret\n");
-    return 0;
 }

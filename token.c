eb72393fc094edd22cbbf4246c4a53ee3f9570fb
Toru Takefusa <torutakefusa@gmail.com>
2023-10-25 19:50:00 +0900

    Step17 不要な記載の削除

--- token.c
+++ token.c
@@ -55,31 +55,31 @@ void *tokenize() {
             continue;
         }
 
-        if (strncmp(p, "if", 2) == 0 && !is_alnum(p[2])) {
+        if (!strncmp(p, "if", 2) && !is_alnum(p[2])) {
             cur = new_token(TK_IF, cur, p, 2);
             p += 2;
             continue;
         }
 
-        if (strncmp(p, "else", 4) == 0 && !is_alnum(p[4])) {
+        if (!strncmp(p, "else", 4) && !is_alnum(p[4])) {
             cur = new_token(TK_ELSE, cur, p, 4);
             p += 4;
             continue;
         }
 
-        if (strncmp(p, "while", 5) == 0 && !is_alnum(p[5])) {
+        if (!strncmp(p, "while", 5) && !is_alnum(p[5])) {
             cur = new_token(TK_WHILE, cur, p , 5);
             p += 5;
             continue;
         }
 
-        if (strncmp(p, "for", 3) == 0 && !is_alnum(p[3])) {
+        if (!strncmp(p, "for", 3) && !is_alnum(p[3])) {
             cur = new_token(TK_FOR, cur, p, 3);
             p += 3;
             continue;
         }
 
-        if (strncmp(p, "return", 6) == 0 && !is_alnum(p[6])) {
+        if (!strncmp(p, "return", 6) && !is_alnum(p[6])) {
             cur = new_token(TK_RETURN, cur, p, 6);
             p += 6;
             continue;
